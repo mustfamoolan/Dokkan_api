ae783899b35aca13150feb040962ebb98f7ac9b5
mustfamoolan <almoq3m@gmail.com>
2026-02-07 15:52:37 +0300

    Fix duplicate migrations causing server crash

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreProductRequest;
 use App\Models\Product;
+use App\Models\ProductSupplier;
 use App\Http\Resources\ProductResource;
 use Illuminate\Http\Request;
 
