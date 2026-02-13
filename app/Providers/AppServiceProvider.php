63e6ccf84007495d99cf2b065ccbde0e4ac7a7eb
mustfamoolan <almoq3m@gmail.com>
2026-02-13 04:18:07 +0300

    Fix AppServiceProvider and filesystems config

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,12 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        \App\Models\PurchaseInvoice::observe(\App\Observers\PurchaseInvoiceObserver::class);
-        \App\Models\PurchaseReturn::observe(\App\Observers\PurchaseReturnObserver::class);
-        \App\Models\SalesInvoice::observe(\App\Observers\SalesInvoiceObserver::class);
-        \App\Models\SalesReturn::observe(\App\Observers\SalesReturnObserver::class);
-        \App\Models\Receipt::observe(\App\Observers\ReceiptObserver::class);
-        \App\Models\Payment::observe(\App\Observers\PaymentObserver::class);
-        \App\Models\PayrollRun::observe(\App\Observers\PayrollRunObserver::class);
+        //
     }
 }
