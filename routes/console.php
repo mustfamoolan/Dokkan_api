6a23d1dced5c9e2786d2f7bca48c1a1f516d3126
mustfamoolan <almoq3m@gmail.com>
2026-02-13 04:20:20 +0300

    Remove missing CheckExpiredBatches job from console schedule

--- routes/console.php
+++ routes/console.php
@@ -3,11 +3,5 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
-use App\Jobs\CheckExpiredBatches;
-
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
-
 // Schedule daily check for expired batches
-Schedule::job(new CheckExpiredBatches)->daily();
+// Schedule::job(new CheckExpiredBatches)->daily();
